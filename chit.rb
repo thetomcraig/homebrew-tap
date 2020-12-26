3a8e16f3d9f13ddbdccec42839d5f35f211ab275
Tom Craig <thetomcraig@icloud.com>
2020-12-25 22:34:48 -0800

    update to 1.1

--- chit.rb
+++ chit.rb
@@ -1,8 +1,8 @@
 class Chit < Formula
   desc "Change your terminal color schemes"
   homepage "https://github.com/thetomcraig/Chit"
-  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.1.0.tar.gz?raw=true"
-  sha256 "84052f9f747b0cb207173d576ac77d0ee90273a0a9108922be0d81be9821698e"
+  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.1.1.tar.gz?raw=true"
+  sha256 "912a83782ee27de81858519742ff0824722b70bb1fae75dad15f5b63ac24682e"
   license "BSD-3-Clause"
 
   bottle :unneeded
