ad6732e3da0679da25a62b20f055d9f8b41caa75
Tom Craig <thetomcraig@icloud.com>
2021-06-23 16:49:23 -0700

    reverting to before configuration change

--- chit.rb
+++ chit.rb
@@ -3,8 +3,8 @@ class Chit < Formula
   CONFIG_DIR = "${HOME}/.config/chit"
   desc "Change your terminal color schemes"
   homepage "https://github.com/thetomcraig/Chit"
-  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.2.0.tar.gz?raw=true"
-  sha256 "cf758c116532584ee87ca8f9919205b3541ab1c7789d2a8377ea7769d3c4d158"
+  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.2.1.tar.gz?raw=true"
+  sha256 "cca4397b9bb18f7cb254c99a7d226bf01e1aee0c789e5a0ca4be1d30d5a8c584"
   license "MIT"
 
   bottle :unneeded
@@ -12,26 +12,11 @@ class Chit < Formula
   def install
     system "./build"
     bin.install "chit"
-
-    system "./configure"
   end
 
   def post_install
-    # WIP write this to replace the "setup" command
-    #
-    #mkdir_p "#{var}/log/nexus" unless (var/"log/nexus").exist?
-    #
-    # mkdir_p "#{CONFIG_DIR}/theme_definitions"
-    # mkdir_p "#{CONFIG_DIR}/kitty_themes"
-
-    # system "cp", "example_theme_definitions", CONFIG_DIR/"theme_definitions"
-    # system "cp", "kitty_themes", CONFIG_DIR/"kitty_themes"
-
-    # system "echo", ">", "'dark'", CONFIG_DIR/"currrent_theme"
-    # system "echo", ">", "''", CONFIG_DIR/"tmux_theme.conf"
-
-    #etc.install "example_theme_definitions" => CONFIG_DIR/"theme_definitions"
-    #etc.install "kitty_themes" => CONFIG_DIR/"kitty_themes"
+    etc.install "example_theme_definitions" => CONFIG_DIR/"theme_definitions"
+    etc.install "kitty_themes" => CONFIG_DIR/"kitty_themes"
   end
 
 end
