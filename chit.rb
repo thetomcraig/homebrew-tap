40d3a962a0640046470cf9d115de8365d113b7df
Tom Craig <thetomcraig@icloud.com>
2021-06-23 08:39:53 -0700

    update to 0.1.6

--- chit.rb
+++ chit.rb
@@ -1,8 +1,10 @@
+
 class Chit < Formula
+  CONFIG_DIR = "${HOME}/.config/chit"
   desc "Change your terminal color schemes"
   homepage "https://github.com/thetomcraig/Chit"
-  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.1.4.tar.gz?raw=true"
-  sha256 "9ae359848133563a62678c0d95dc65736dd826f55a9ab1990b736b5a2166377e"
+  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.1.6.tar.gz?raw=true"
+  sha256 "2133faf798269a7ba291aaadcd9c9ef568b2b11c20914c0455bc2b44c2edae74"
   license "MIT"
 
   bottle :unneeded
@@ -10,10 +12,24 @@ class Chit < Formula
   def install
     system "./build"
     bin.install "chit"
-    # Folder to hold the example thome .conf files
-    # When initializing chit, these are copied to ~/.config/chit/theme_definitions/examples
-    etc.install "example_theme_definitions" => "chit/example_theme_definitions"
-    etc.install "kitty_themes" => "chit/kitty_themes"
   end
-end
 
+  def post_install
+    # WIP write this to replace the "setup" command
+    #
+    CONFIG_DIR.mkpath
+    (CONFIG_DIR/"theme_definitions").mkpath
+    (CONFIG_DIR/"kitty_themes").mkpath
+
+
+    system "cp", "example_theme_definitions", CONFIG_DIR/"theme_definitions"
+    system "cp", "kitty_themes", CONFIG_DIR/"kitty_themes"
+
+
+    system "echo", ">", "'dark'", CONFIG_DIR/"currrent_theme"
+    system "echo", ">", "''", CONFIG_DIR/"tmux_theme.conf"
+    #etc.install "example_theme_definitions" => CONFIG_DIR/"theme_definitions"
+    #etc.install "kitty_themes" => CONFIG_DIR/"kitty_themes"
+  end
+
+end
