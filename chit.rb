0529c1a403bbd2f66de1615b66577aa156c44fa2
Tom Craig <thetomcraig@icloud.com>
2021-10-09 22:09:18 -0700

    0.2.9; testing the install/setup steps

--- chit.rb
+++ chit.rb
@@ -2,17 +2,19 @@
 class Chit < Formula
   desc "Change your terminal color schemes"
   homepage "https://github.com/thetomcraig/Chit"
-  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.2.8.tar.gz?raw=true"
-  sha256 "7d9158013786e8b6c928d49cc9b863c67b017adff8acb63566524bbcf529f4ce"
+  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.2.9.tar.gz?raw=true"
+  sha256 "2bd506ce5c6baa9feea563b4ff47b446ee6f9b5b26e89f01c610333bd31c0211"
   license "MIT"
 
   bottle :unneeded
 
   def install
+    # Install binary
     bin.install "chit.sh" => "chit"
-    etc.install "example_theme_definitions" => "chit/theme_definitions"
-    # mkdir_p theme_definitions
-    # cp_r "example_theme_definitions", theme_definitions 
+    # Install the example themes into share,
+    # so they will be removed on formula uninstall
+    share.install "example_theme_definitions"
+    # The user-defined themes will live in the main ~/.config folder
   end
 
 end
